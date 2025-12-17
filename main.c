cae8fa9f53ac32ccec2687d7e7ec03991bec9f79
Michał Przyłucki <michal.przyl@gmail.com>
2025-12-17 19:42:30 +0100

    more functionality

--- main.c
+++ main.c
@@ -38,10 +38,61 @@ Node* create_node(const char* name, NodeType type){
   return node;
 }
 
-int main(){
-  Node *root = create_node("/", NODE_DIR);
 
-  printf("root: %s\n", root->name);
-  free(root);
+void add_child(Node* parent, Node*child){
+  if (!parent || !child) return;
+
+  if (parent->type != NODE_DIR){
+    printf("Error: Cannot add child to file '%s'\n", parent->name);
+    return;
+  }
+
+  child->parent = parent;
+
+  child->next = parent->children;
+  parent->children = child;
+
+}
+
+
+void free_tree(Node* node){
+  if (!node) return;
+  Node* curr = node->children;
+  while (curr != NULL){
+    Node* next = curr->next;
+    free_tree(curr);
+    curr = next;
+  }
+
+  free(node);
+  
+}
+
+int main(){
+  Node* root = create_node("/", NODE_DIR);
+  Node* home = create_node("home", NODE_DIR);
+  Node* user = create_node("user", NODE_DIR);
+  Node* file = create_node("notes.txt", NODE_FILE);
+  Node* file_2 = create_node("games.txt", NODE_FILE);
+  Node* file_3 = create_node("secret.txt", NODE_FILE);
+
+  add_child(root, home);
+  add_child(home, user);
+  add_child(user, file);
+  add_child(user, file_2);
+  add_child(user, file_3);
+
+  printf("root -> %s\n", root->children->name);
+  printf("home -> %s\n", home->children->name);
+
+  // temporary pointer
+  Node *curr = user->children;
+  
+  while(curr != NULL){
+    printf("user -> %s\n", curr->name);
+    curr = curr->next;
+  }
+
+  free_tree(root);
   return 0;
 }
